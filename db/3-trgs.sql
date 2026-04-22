470595ad71393cae66292e599fdf92a2f30d0b62
Yael <243678@ids.upchiapas.edu.mx>
2026-04-21 22:48:44 -0600

    ya funciona el notice, entre que era con una comilla y que no le puse punto y coma no se cual es peor

--- db/3-trgs.sql
+++ db/3-trgs.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION crear_perfil()
 RETURNS TRIGGER AS $$
 BEGIN
-    RAISE NOTICE "Creando perfil..."
+    RAISE NOTICE 'Creando perfil...';
     INSERT INTO perfiles (id_usuario) VALUES (NEW.id_usuario);
     RETURN NEW;
 END;
@@ -10,4 +10,4 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER trg_crear_perfil_usuario
 AFTER INSERT ON usuarios
 FOR EACH ROW
-EXECUTE FUNCTION crear_perfil_automatico();
+EXECUTE FUNCTION crear_perfil();
