f24ab37260aa78600c1d8708389ceae91bb13536
Yael <243678@ids.upchiapas.edu.mx>
2026-04-22 01:41:32 -0600

    mas rls

--- db/5-rls.sql
+++ db/5-rls.sql
@@ -1,5 +1,23 @@
 ALTER TABLE obras ENABLE ROW LEVEL SECURITY;
 
 CREATE POLICY lectura_obras ON obras FOR SELECT USING (true);
-
 CREATE POLICY edicion_obras ON obras FOR UPDATE USING (id_usuario = get_current_id()) WITH CHECK (id_usuario = get_current_id());
+CREATE POLICY borrado_obras ON obras FOR DELETE USING (id_usuario = get_current_id());
+
+
+ALTER TABLE perfiles ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY edicion_perfil ON perfiles FOR UPDATE USING (id_usuario = get_current_id());
+CREATE POLICY borrado_perfil ON perfiles FOR DELETE USING (id_usuario = get_current_id());
+
+
+ALTER TABLE comentarios ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY edicion_comentario ON comentarios FOR UPDATE USING (id_usuario = get_current_id());
+CREATE POLICY borrado_comentario ON comentarios FOR DELETE USING (id_usuario = get_current_id());
+
+
+ALTER TABLE respuestas_comentario ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY edicion_respuesta ON respuestas_comentario FOR UPDATE USING (id_usuario = get_current_id());
+CREATE POLICY borrado_respuesta ON respuestas_comentario FOR DELETE USING (id_usuario = get_current_id());
